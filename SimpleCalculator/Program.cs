5b17a33e34845977f25a700b1353b3b58cc8e512
Emil <Emil@LAPTOP-PV56MM27>
2021-02-20 16:54:21 +0100

    initiate

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace Calculator
 {
     /*
@@ -26,7 +25,50 @@ namespace Calculator
             Console.WriteLine("Please, enter your seccond number:");
             int input_2 = Convert.ToInt32(Console.ReadLine());
             int result = 0;
+            switch (action) {
+                case 1: {
+                    result = Addition(input_1, input_2);
+                    break;
+                } 
+                case 2: {
+                    result = Substraction(input_1, input_2);
+                    break;
+                }
+                case 3: {
+                    result = Multiplication(input_1, input_2);
+                    break;
+                }
+                case 4: {
+                    result = Division(input_1, input_2);
+                    break;
+                }
+                default:
+                    Console.WriteLine("Invalid entry");
+                    break;
+            }
+            Console.WriteLine(" = {0}",result);
+            Console.ReadKey();
 
+        }
+        //Addition
+        public static int Addition(int input_1, int input_2) {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Substraction
+        public static int Substraction(int input_1, int input_2) {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Multiplication
+        public static int Multiplication(int input_1, int input_2) {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Division
+        public static int Division(int input_1, int input_2) {
+            int result = input_1 + input_2;
+            return result;
         }
     }
 }
